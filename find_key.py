8f2c20f5a5f6665125051c89021913f5338b8af6
unknown <gregmalczynski@gmail.com>
2025-09-04 21:43:41 +0200

    Improve game/code

--- find_key.py
+++ find_key.py
@@ -1,70 +1,81 @@
-# Simple Game *** FIND KEY ***
-
-# Imports
 from random import randint
-from math import sqrt
+import os
 
-# Declare game data
+CRED = '\033[91m'
+CYEL = '\033[43m'
+CEND = '\033[0m'
 
-GAME_WIDTH = 10
-GAME_HEIGHT = 10
+X_SIZE = 10
+Y_SIZE = 10
 
-key_x = randint(0, GAME_WIDTH)
-key_y = randint(0, GAME_HEIGHT)
+x_key = randint(0, X_SIZE)
+y_key = randint(0, Y_SIZE)
 
-player_x = 0
-player_y = 0
+x_player = 0
+y_player = 0
 
 player_found_key = False
 
 moves = 0
-distance_before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2 )
+
+array = [
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+    [' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + ', ' + '],
+]
 
 while not player_found_key:
-    moves += 1
+    os.system('cls')
+
+    for i in range(0, X_SIZE):
+        for j in range(0, Y_SIZE):
+            
+            player_position = f'{x_player}{y_player}'
+            if i == x_player and j == y_player:
+                print(CYEL + ' P ', end='' + CEND)
+            elif i == x_key and j == y_key:
+                print(CRED + ' K ', end='' + CEND)
+            else:
+                print(array[i][j], end='')
+        print()
+
     print()
+    print(f'        Moves: {moves}')
+    print(f'x : {x_player}, y : {y_player}')
     print('You can go w/a/s/d')
-
     move = input('Where U ant to go ?')
-
     match move.lower():
         case 'w':
-            player_x += 1
-            if player_x > GAME_HEIGHT:
+            x_player -= 1
+            if x_player < 0:
                 print('Wall hit!!!')
-                player_x = GAME_HEIGHT
+                x_player = 0
         case 's':
-            player_x -= 1
-            if player_x < 0:
+            x_player += 1
+            if x_player > X_SIZE - 1:
                 print('Wall hit!!!')
-                player_x = 0
+                x_player = X_SIZE - 1
         case 'a':
-            player_y -= 1
-            if player_y < 0:
+            y_player -= 1
+            if y_player < 0:
                 print('Wall hit!!!')
-                player_y = 0
+                y_player = 0
         case 'd':
-            player_y += 1
-            if player_y > GAME_WIDTH:
+            y_player += 1
+            if y_player > Y_SIZE - 1:
                 print('Wall hit!!!')
-                player_y = GAME_WIDTH
-        case 'q':
-            print('Quit game')
-            quit()
-        case '_':
-            print('I dont know where U want to go')
-            continue
+                y_player = Y_SIZE - 1
+    
+    if x_player == x_key and y_player == y_key:
+        player_found_key = True
+        os.system('cls')
+        print(f'GAME END\nYou found the key!\n')
 
-    if player_x == key_x and player_y == key_y:
-        print(f'Congratulations, U found key in {moves} times!')
-        quit()
-
-    distance_after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2 )
-    if distance_before_move > distance_after_move:
-        print('You are nearer')
-    else:
-        print('You are moving away')
-
-    distance_before_move = distance_after_move
-
-    print(f'Your current position is x : {player_x} , y : {player_y}')
+    moves += 1
